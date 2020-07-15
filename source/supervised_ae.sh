5f44ad2c45116ff4f7b03f3fad22fd0d09635f20
lstruski <struski@ii.uj.edu.pl>
2020-07-15 11:50:03 +0200

    update

--- source/supervised_ae.sh
+++ source/supervised_ae.sh
@@ -3,22 +3,47 @@
 dirs=(data)
 targets=(5HT2A 5HT2c 5HT6 D2 HIVint HIVprot HIVrev NMDA NOP NPC1 catB catL kappa mi)
 
-outdir=/mnt/users/struski/local/chemia/new_01_07_2020/inne_fingerprinty/supervised_ae_$(date +%d-%m-%Y)
+if [[ "$1" == "" ]]; then
+    current_date=$(date +%d-%m-%Y)
+else
+    current_date=$1
+fi
+outdir=/mnt/users/struski/local/chemia/new_01_07_2020/inne_fingerprinty/supervised_ae_${current_date}
 
+ae_model="7000 4000 500 1000 100"
+classifier_model=("100" "100 25" "100 50 20 10")
 
-for d in ${dirs[@]}; do
-    for t in ${targets[@]}; do
-        if [[ ! -f "${outdir}/${d}/${t}.txt" ]]; then
-            echo -e "\033[0;1;31mDIR: ${d}\t \033[0;1;33mTARGET: ${t}\033[0m"
-#            command="nice -9 taskset -c 46-70 python supervised_ae.py --data_dir ./data/${d} --name ${t} --pretrain_epochs 75 --epochs 100 --dims_layers_ae 7000 4000 500 1000 100 --dims_layers_classifier 100 50 20 10 --batch_size 50 --lr 0.0001 --save_dir ${outdir}/${d} --use_dropout --procedure pre-training_ae training_classifier --scale_loss 1"
+if [[ "$2" == "" ]]; then
 
-            command="nice -9 taskset -c 46-70 python supervised_ae.py --data_dir ./data/${d} --name ${t} --pretrain_epochs 75 --epochs 100 --dims_layers_ae 7000 4000 500 1000 100 --dims_layers_classifier 100 --batch_size 50 --lr 0.0001 --save_dir ${outdir}/${d} --use_dropout --procedure pre-training_ae training_classifier --scale_loss 1"
-
-            eval ${command}
-            if [[ ! -f "${outdir}/commands_used.txt" ]]; then
-                echo ${command} > ${outdir}/commands_used.txt
-            fi
+    for sc in 1 1.5 2 4; do for lr in 0.0001 0.00001; do for ep in 100 75 50 25 0; do for clr_model in ${classifier_model[@]}; do
+        add2output=lr_${lr}-pretrnEpoch_${ep}-scale_${sc}-clr_${clr_model// /-}
+        proc="pre-training_ae training_all"
+        if [[ "${ep}" == "0" ]]; then
+            proc="training_all"
         fi
-    done
-done
 
+        for d in ${dirs[@]}; do
+            for t in ${targets[@]}; do
+                if [[ ! -f "${outdir}/${add2output}/${d}/${t}.txt" ]]; then
+                    echo -e "\033[0;1;31mDIR: ${d}\t \033[0;1;33mTARGET: ${t}\033[0m"
+
+                    command="python supervised_ae.py --data_dir ./data/${d} --name ${t} --pretrain_epochs ${ep} --epochs 150 --dims_layers_ae ${ae_model}  --dims_layers_classifier ${clr_model} --batch_size 50 --lr ${lr} --save_dir ${outdir}/${add2output}/${d} --use_dropout --procedure ${proc} --scale_loss ${sc}"
+
+                    echo -e "\033[1;31mRUN: \033[0;1;32m${command}\033[0m"
+                    eval ${command}
+                    if [[ ! -f "${outdir}/${add2output}/commands_used.txt" ]]; then
+                        echo ${command} > ${outdir}/${add2output}/commands_used.txt
+                    fi
+                fi
+            done
+
+            ./results2csv.sh ${outdir}/${add2output}/${d} ${outdir}/${add2output} 2
+        done
+
+    done;done;done;done
+
+else
+
+    exit
+
+fi
